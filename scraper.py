9b06d5e38f6c5085114b301c6ecad9d239b8cad9
Arjun Kadam <arjunkadampatil693@gmail.com>
2026-03-09 18:36:38 +0530

    creating new file

---
+++ scraper.py
@@ -0,0 +1,42 @@
+# ------------------------------- #
+# Assignment 1 - Web Page Fetcher #
+# ------------------------------- #
+
+
+# required libraries
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+if len(sys.argv) < 2:
+    print("give your web url in the command line")
+    exit()
+
+weburl = sys.argv[1]
+
+try:
+    dummyAccessProviders = {"User-Agent": "Mozilla/5.0"}
+    r = requests.get(weburl, headers=dummyAccessProviders)
+except:
+    print("error in fetching the page")
+    exit()
+
+htmls = r.text
+s = BeautifulSoup(htmls, 'html.parser')
+
+# title
+if s.title:
+    print("TITLE:")
+    print(s.title.get_text().strip())
+
+# body
+print("\nBODY:")
+if s.body:
+    print(s.body.get_text(separator=' ', strip=True))
+
+# links
+print("\nLINKS:")
+for i in s.find_all('a'):
+    links = i.get('href')
+    if links:
+        print(links)
